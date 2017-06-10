64b95892556085c837c3742ee387172f5703427a
JacksonSRodrigues <jackson.sunny.rodrigues@gmail.com>
2017-06-11 00:36:30 +0530

    update project to electron app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { ColorSegregator, HierarchyGenerator } from './image-processor'
 import tImage from './test.png'
 import { HtmlRenderer } from './renderer/html'
@@ -41,21 +38,6 @@ class App extends Component {
     }).catch(err => {
       console.log('Error', err);
     })
-  }
-
-  render() {
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
   }
 }
 
